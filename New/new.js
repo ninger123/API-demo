9193729dd4ba9db7cff2d6ad8bfec8458e67adc7
ninger <1106697802@qq.com>
2018-12-17 17:59:59 +0800

    new和构造函数

---
+++ New/new.js
@@ -0,0 +1,17 @@
+/*
+ 1.human 这个对象本身具有属性 name 和 city
+ 2.human.__proto__ 对应的对象（也就是原型）具有物种（species）、走（walk）和使用工具（useTools）这几个属性
+ 3.human.__proto__.constructor === Human 为 true
+ human2 和 human 类似。
+ */
+function Human(options){
+    this.name =options.name;
+    this.city =options.city;
+} // 构造函数结束
+
+Human.prototype.species ="human";
+Human.prototype.walk = function(){};
+Human.prototype.useTools=function(){};
+
+var human = new Human({name:'Frank', city: 'Hangzhou'});
+var human2 = new Human({name:'Jack', city: 'Hangzhou'});
